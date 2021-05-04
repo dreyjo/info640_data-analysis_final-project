7ade360fd79b0fcaef87002b754e5270231946f8
Drey Jo <ajonatha@pratt.edu>
2021-05-04 14:51:48 -0400

    added trial plots, tried tf-idf, added notes on stemming completion

--- data/data_cleaning_to_lda.R
+++ data/data_cleaning_to_lda.R
@@ -100,6 +100,7 @@ print(papers_dtm[[1]])
 #---following stackoverflow code which includes stemming and stem completion:
 #don't understand this code and need to ask Prof McSweeny about it
 papers_tdm <- TermDocumentMatrix(papers_data, control = list(stemming = TRUE)) 
+#use an assigned variable so you can refer to completed
 cbind(stems = rownames(papers_tdm), completed = stemCompletion(rownames(papers_tdm), papers_data))
 
 #checking data type and class type
@@ -174,7 +175,7 @@ top_terms_lda_2 %>%
 papers_dtm_tf <- DocumentTermMatrix(papers_data, control = list(weighting = weightTfIdf))
 papers_dtm_tf
 
-#Analyse how frequently terms appear by summing the content of all terms (i.e., rows).
+#Analyze how frequently terms appear by summing the content of all terms (i.e., rows).
 freq=colSums(as.matrix(papers_dtm_tf))
 head(freq,10)
 
@@ -182,8 +183,47 @@ head(freq,10)
 #question why tail?
 tail(sort(freq),n=10)
 
+#plotting top frequent terms:
+high.freq=tail(sort(freq),n=10)
+hfp.df=as.data.frame(sort(high.freq))
+hfp.df$names <- rownames(hfp.df) 
+
+ggplot(hfp.df, aes(reorder(names,high.freq), high.freq)) +
+  geom_bar(stat="identity") + coord_flip() + 
+  xlab("Terms") + ylab("Frequency") +
+  ggtitle("Term frequencies")
+
+#trying to remove most frequent terms from papers dtm
+install.packages("udpipe")
+library(udpipe)
+freq_terms <- c("accountability", "models", "recourse", "systems", "algorithmic", "trust", "data", "explanations", "model", "fairness")
+
+#----------QUESTION FROM HERE:
+#why didn't stemming work? or rather how and when do use stemCompletion? before or after DTM?
+#why are frequent words I already removed like data and fairness still showing up as most frequent? 
+papers_data_frq <- tm_map(papers_data, removeWords, freq_terms)
+print(papers_data_frq[[10]][1])
+papers_dtm_frq <- DocumentTermMatrix(papers_data)
+
+freq_2<-colSums(as.matrix(papers_dtm_frq))
+tail(sort(freq_2),n=10)
+
+#tf-idf doesn't seem to change anything and causes issues
+#With advice from Prof.McSweeney:
+
+#-Next way to see if I get different reseults would be in incoporating stemmed results
+#--set the table of stem and stemcompleted to a variable so you can reference a column if need be
+#so trial 3 is with a STEMMED papers_dtm not weighted with tf-idf. 
+
+#analysis could begin with understadning:
+#- what terms appear in some topics and which don't? If data appears in 2/3 topics why might that be?
+
+#- if terms appear frequently across topics why might that be? Could those terms be used in different ways within those topics?
+#--one way to analyze this might be reconnecting topics to paper sources/ids:
+#---specifically pulling out which papers are great representations of each topic, looking at these papers to see what they have in common in-topic? 
+#---and seeing if these words are being deployed in the same way
 k3 <- 3
-papers_lda_3 <- LDA(papers_dtm_tf, k = k3, control = list(seed=1234)) 
+papers_lda_3 <- LDA(papers_dtm, k = k3, control = list(seed=1234)) 
 papers_lda_3 
 papers_lda_3_terms <- terms(papers_lda_3,5)
 
